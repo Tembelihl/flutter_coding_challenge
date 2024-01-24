5d042c532df3cb1de0620b6d67a5d8ec5522a15a
Tembelihle Biyana <127286965+Tembelihl@users.noreply.github.com>
2024-01-24 20:52:13 +0200

    Add an AppBar and a colour

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,21 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp2());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp2 extends StatelessWidget {
+  const MyApp2({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SplashScreen(), // Show SplashScreen initially
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    // Add a delay for 3.5 seconds to simulate the splash screen duration
-    Timer(Duration(seconds: 3), () {
-      // Navigate to the home screen after the splash screen duration
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => HomeScreen()),
-      );
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        // Display the Quickloc8 logo
-        child: Image.asset('assets/Quickloc8-logo.png'),
-      ),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        // Your main screen content goes here
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+        ),
       ),
+
     );
   }
 }
